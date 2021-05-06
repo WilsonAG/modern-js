95891f1ee1dc45bc67436260379aa6b5bfb5c3dd
Wilson Aguilar <waguilars@est.ups.edu.ec>
2021-05-06 17:58:15 -0500

    pryecto filtro busquedas

---
+++ 17-PROYECTO-Buscador/js/app.js
@@ -0,0 +1,184 @@
+
+const marca = document.querySelector('#marca')
+const year = document.querySelector('#year')
+const minimo = document.querySelector('#minimo')
+const maximo = document.querySelector('#maximo')
+const puertas = document.querySelector('#puertas')
+const transmision = document.querySelector('#transmision')
+const color = document.querySelector('#color')
+const resultado = document.querySelector('#resultado')
+
+const max =  new Date().getFullYear();
+const min = max - 10;
+
+const datosBusqueda = {
+  marca: '',
+  year: '',
+  minimo: '',
+  maximo: '',
+  puertas: '',
+  transmision: '',
+  color: ''
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  mostrarAutos(autos);
+
+  llenarSelect();
+})
+
+marca.addEventListener('change', (e) => {
+  datosBusqueda.marca = e.target.value
+
+  filtrarAuto();
+})
+
+year.addEventListener('change', (e) => {
+  datosBusqueda.year = e.target.value
+  filtrarAuto();
+})
+
+minimo.addEventListener('change', (e) => {
+  datosBusqueda.minimo = e.target.value
+  filtrarAuto();
+})
+
+maximo.addEventListener('change', (e) => {
+  datosBusqueda.maximo = e.target.value
+  filtrarAuto();
+})
+
+puertas.addEventListener('change', (e) => {
+  datosBusqueda.puertas = e.target.value
+  filtrarAuto();
+})
+
+transmision.addEventListener('change', (e) => {
+  datosBusqueda.transmision = e.target.value
+  filtrarAuto();
+})
+
+color.addEventListener('change', (e) => {
+  datosBusqueda.color = e.target.value
+  filtrarAuto();
+})
+
+
+
+function mostrarAutos(autos) {
+  limpiarHTML()
+
+  autos.forEach(auto => {
+    const { marca, modelo, year, precio, puertas, color, transmision} = auto
+    const autoHTML = document.createElement('p')
+
+    autoHTML.textContent = `
+      ${marca} ${modelo} - ${year} - ${puertas} Puertas - Transmicion: ${transmision} - Precio: ${precio} - Color: ${color}
+    `
+
+    resultado.appendChild(autoHTML)
+  })
+}
+
+function limpiarHTML () {
+  while(resultado.firstChild) {
+    resultado.removeChild(resultado.firstChild)
+  }
+}
+
+function llenarSelect() {
+  const anios = [...new Set(autos.map(auto => auto.year))].sort()
+  anios.forEach(anio => {
+    const option = document.createElement('option')
+    option.value = anio
+    option.textContent = anio
+    year.appendChild(option)
+  })
+}
+
+function filtrarAuto() {
+  const resultado = autos
+    .filter( filtrarMarca )
+    .filter( filtrarYear )
+    .filter( filtrarMinimo )
+    .filter( filtrarMaximo )
+    .filter( filtrarPuertas )
+    .filter( filtrarTransmicion )
+    .filter( filtrarColor )
+
+  if (resultado.length) {
+    mostrarAutos(resultado)
+  } else {
+    noResultado()
+  }
+}
+
+function noResultado () {
+  limpiarHTML()
+  const noResultado = document.createElement('div')
+  noResultado.classList.add('alerta', 'error')
+  noResultado.textContent = 'No hay resultados'
+  resultado.appendChild(noResultado)
+}
+
+function filtrarMarca(auto) {
+  if ( datosBusqueda.marca ) {
+    return auto.marca === datosBusqueda.marca
+  }
+
+  return auto
+}
+
+function filtrarYear(auto) {
+  if ( datosBusqueda.year ) {
+    return +auto.year === +datosBusqueda.year
+  }
+
+  return auto
+}
+
+function filtrarMinimo(auto) {
+  const { minimo } = datosBusqueda
+  if (minimo) {
+    return +auto.precio >= +minimo
+  }
+
+  return auto
+}
+
+function filtrarMaximo(auto) {
+  const { maximo } = datosBusqueda
+  if (maximo) {
+    return auto.precio <= maximo
+  }
+
+  return auto
+}
+
+function filtrarPuertas (auto) {
+  const { puertas } = datosBusqueda
+  if (puertas) {
+    return +auto.puertas === +puertas
+  }
+
+  return auto
+}
+
+function filtrarTransmicion (auto) {
+  const { transmision } = datosBusqueda
+  if (transmision) {
+    return auto.transmision === transmision
+  }
+
+  return auto
+}
+
+function filtrarColor (auto) {
+  const { color } = datosBusqueda
+  if (color) {
+    return auto.color === color
+  }
+
+  return auto
+}
